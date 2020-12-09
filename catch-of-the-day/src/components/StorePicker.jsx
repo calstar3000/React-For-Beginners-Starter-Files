701031540254251c40917c8d7a3a9b9fa0613f0f
Calum O'Neil <calum.oneil@gmail.com>
2020-12-10 10:32:21 +1300

    Basic routing

--- catch-of-the-day/src/components/StorePicker.jsx
+++ catch-of-the-day/src/components/StorePicker.jsx
@@ -1,11 +1,29 @@
 import React from "react";
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+  storeNameInput = React.createRef();
+
+  goToStore = event => {
+    // 1. Stop the form from submitting
+    event.preventDefault();
+    // 2. Get the text from that input
+    const storeName = this.storeNameInput.current.value;
+    // 3. Change the page to /store/<whatever-they-entered>
+    this.props.history.push(`/store/${storeName}`);
+  };
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input
+          type="text"
+          ref={this.storeNameInput}
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+        />
         <button type="submit">Visit Store →</button>
       </form>
     );
